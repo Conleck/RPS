20d3e78a3e951049a5205ded8e4bcefcfd956299
Conor Leckey <conorl@kainos.com>
2017-10-02 16:43:28 +0100

    First commit

---
+++ src/com/company/Player.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Player {
+    private String name;
+    private int wins, losses;
+
+    public Player(String name){
+        this.name = name;
+        wins = 0;
+        losses = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+}
+
+
