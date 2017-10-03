964e1e784affb7ec85911593300685a33b365836
Steven Foreman <stevenf@kainos.com>
2017-10-03 11:25:29 +0100

    Changed the game class and created the start game method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
     }
 
     public static void showScore(){
@@ -22,6 +21,5 @@ public class Main {
     public static void createPlayer(){
 
 
-        Player
     }
 }
